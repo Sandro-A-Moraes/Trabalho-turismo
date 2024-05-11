de10d7c95472dd489f5d428426c0b8077f282a63
Sandro-A-Moraes <163610328+Sandro-A-Moraes@users.noreply.github.com>
2024-05-11 18:12:45 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
-function onClickMenu(){
-    document.getElementById("menu").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-}
+// Animação menu
+document.querySelector(".hamburguer").addEventListener("click", () => document.querySelector(".container").classList.toggle("show-menu"));
+
+
+
+let sectionTeste = document.querySelector("section.mais__visitados");
+        sectionTeste.querySelectorAll(".container .item").forEach(item => {
+            let label = item.querySelector("label");
+            let input = item.querySelector("input");
+
+            label.addEventListener("mouseover", () => {
+                input.checked = true;
+            })
+        })
