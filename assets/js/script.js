a4f62a61c43c20e344539f69f04a3549d838f6ca
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2024-05-11 18:04:53 -0300

    pull carrossel and add menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+// Animação menu
 document.querySelector(".hamburguer").addEventListener("click", () => document.querySelector(".container").classList.toggle("show-menu"));
+
+
+
+let sectionTeste = document.querySelector("section.mais__visitados");
+        sectionTeste.querySelectorAll(".container .item").forEach(item => {
+            let label = item.querySelector("label");
+            let input = item.querySelector("input");
+
+            label.addEventListener("mouseover", () => {
+                input.checked = true;
+            })
+        })
