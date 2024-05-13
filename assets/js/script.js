49a1d80fdbc5cb0f8f21474be4cc79232640c9fd
Sandro-A-Moraes <sandro.corporativo3004@outlook.com>
2024-05-13 00:57:01 -0300

    end menu-itens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
-// Animação menu
-document.querySelector(".hamburguer").addEventListener("click", () => document.querySelector(".container").classList.toggle("show-menu"));
+// ANIMAÇÕES DO MENU (NÃO MEXER)
 
-document.querySelector(".hamburguer").addEventListener("click", () => document.querySelector(".fundo-vidro").classList.toggle("efeito-vidro")  );
+document.querySelector(".menu-hamburguer").addEventListener("click", () => document.querySelector(".container").classList.toggle("show-menu"));
+document.querySelector(".menu-hamburguer").addEventListener("click", () => document.querySelector(".fundo-vidro").classList.toggle("efeito-vidro"));
+
+// Animações dos itens de menu (tentei uma classe única, mas funcionava apenas para o primeiro item)
+
+document.querySelector("#menu-link-1").addEventListener("click", () => document.querySelector(".container").classList.toggle("show-menu"));
+document.querySelector("#menu-link-1").addEventListener("click", () => document.querySelector(".fundo-vidro").classList.toggle("efeito-vidro"));
+
+document.querySelector("#menu-link-2").addEventListener("click", () => document.querySelector(".container").classList.toggle("show-menu"));
+document.querySelector("#menu-link-2").addEventListener("click", () => document.querySelector(".fundo-vidro").classList.toggle("efeito-vidro"));
+
+document.querySelector("#menu-link-3").addEventListener("click", () => document.querySelector(".container").classList.toggle("show-menu"));
+document.querySelector("#menu-link-3").addEventListener("click", () => document.querySelector(".fundo-vidro").classList.toggle("efeito-vidro"));
 
 
-
-
-
+// Animação dos Mais visitados
 let sectionTeste = document.querySelector("section.mais__visitados");
         sectionTeste.querySelectorAll(".container .item").forEach(item => {
             let label = item.querySelector("label");
